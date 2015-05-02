40668e410991e55ed975d9ac5189d4cec0e11c17
cicidi <cicidi@gmail.com>
2015-05-01 17:33:17 -0700

    mulithread join example

---
+++ MultiThread/src/com/cicidi/multithread/Join.java
@@ -0,0 +1,33 @@
+package com.cicidi.multithread;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Join {
+
+	public static void main(String args[]) throws InterruptedException {
+
+		System.out.println(Thread.currentThread().getName() + " is Started");
+
+		Thread exampleThread = new Thread() {
+			public void run() {
+				try {
+					System.out.println(Thread.currentThread().getName()
+							+ " is Started");
+					Thread.sleep(2000);
+					System.out.println(Thread.currentThread().getName()
+							+ " is Completed");
+				} catch (InterruptedException ex) {
+					Logger.getLogger(Join.class.getName()).log(Level.SEVERE,
+							null, ex);
+				}
+			}
+		};
+
+		exampleThread.start();
+//		exampleThread.join();
+
+		System.out.println(Thread.currentThread().getName() + " is Completed");
+	}
+
+}
