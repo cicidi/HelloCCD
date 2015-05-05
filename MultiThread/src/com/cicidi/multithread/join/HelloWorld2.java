500e016785994549da18abd757518d7caec2b8ca
cicidi <cicidi@gmail.com>
2015-05-05 14:39:38 -0700

    add notify example

---
+++ MultiThread/src/com/cicidi/multithread/join/HelloWorld2.java
@@ -0,0 +1,24 @@
+package com.cicidi.multithread.join;
+
+public class HelloWorld2 implements Runnable {
+	private Thread thread;
+	private String threadMsg;
+
+	public HelloWorld2(String threadMsg) {
+		// TODO Auto-generated constructor stub
+
+		this.threadMsg = threadMsg;
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		this.thread = new Thread();
+		System.out.println(threadMsg);
+	}
+
+	public void start() {
+		this.run();
+	}
+
+}
