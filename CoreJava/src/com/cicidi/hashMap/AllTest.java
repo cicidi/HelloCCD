fa849d1da2fa6551907810dd4ff0f785e60f3e2e
cicidi <cicidi@gmail.com>
2015-05-08 12:43:56 -0700

    hashcode equal method and generic

---
+++ CoreJava/src/com/cicidi/hashMap/AllTest.java
@@ -0,0 +1,18 @@
+package com.cicidi.hashMap;
+
+import static org.junit.Assert.fail;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ SdtHashMapTest.class })
+public class AllTest {
+
+	public void test() {
+
+		fail("Not yet implemented");
+	}
+
+}
---
+++ algorithm/src/com/anagram/Main.java
@@ -0,0 +1,39 @@
+package com.anagram;
+/*
+ *  I use sublime for syntax check, here 
+ *  take care of
+ *  char "C" is lowercase
+ *  toLowerCase method
+ *  string.toCharArray 
+ *  String.valufOf()
+ *  if String, or StringBuilder does not have a subString   index=-1
+ *  
+ *  
+ * */
+public class Main {
+	public static void main(String[] arg) {
+		String str1 = "HelloWorld";
+		String str2 = "HelloWorldd";
+
+		System.out.println((new Main()).isAnagram(str1, str2));
+
+	}
+
+	public boolean isAnagram(String str1, String str2) {
+
+		if (str1 == null || str2 == null || str1.length() != str2.length()) {
+			return false;
+		}
+		char[] array = str1.toLowerCase().toCharArray();
+		StringBuffer sb = new StringBuffer(str2.toLowerCase());
+		int index = -2;
+		for (char a : array) {
+			index = sb.indexOf(String.valueOf(a));
+			if (index == -1) {
+				return false;
+			}
+			sb.delete(index, index + 1);
+		}
+		return true;
+	}
+}
