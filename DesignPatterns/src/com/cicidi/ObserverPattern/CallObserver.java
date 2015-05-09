053f5259e48625c2fc7830c31f4282d8deec9b6f
cicidi <cicidi@gmail.com>
2015-05-08 17:14:05 -0700

    sss

---
+++ DesignPatterns/src/com/cicidi/ObserverPattern/CallObserver.java
@@ -0,0 +1,17 @@
+package com.cicidi.ObserverPattern;
+
+
+public class CallObserver extends Observer {
+	public CallObserver(Subject subject) {
+		this.subject = subject;
+		this.subject.attchObserver(this);
+	}
+
+	@Override
+	public void update() {
+		if (this.subject.getState() > subject.full)
+			System.out.println(subject.getState() + "call cicidi");
+
+	}
+
+}
